c735c0a54b44465599c6ef0e1d282c24a600c666
zhifanjia <allenjia98@gmail.com>
2024-11-14 16:50:49 -0500

    CROS config

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const apiRequest = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 })
 
 export default apiRequest
